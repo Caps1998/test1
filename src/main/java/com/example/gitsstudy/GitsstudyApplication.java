f0d1ea8b09725ab69ba597a2b5ae38a3c69cccb5
Caps1998 <975828542@qq.com>
2020-10-24 16:47:08 +0800

    第一次提交代码

---
+++ src/main/java/com/example/gitsstudy/GitsstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitsstudy/GitsstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
